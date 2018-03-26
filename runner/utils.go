7a40e133b8dec82f1ed91825ae29f7071437b778
Daniil Ivanov <ivanoff.daniil@gmail.com>
2018-03-26 11:55:44 +1100

    Removed logging

--- runner/utils.go
+++ runner/utils.go
@@ -24,14 +24,12 @@ func isTmpDir(path string) bool {
 }
 
 func isIgnoredFolder(path string) bool {
-	watcherLog("path: %v", path)
 	paths := strings.Split(path, "/")
 	if len(paths) <= 0 {
 		return false
 	}
 
 	for _, e := range strings.Split(settings["ignored"], ",") {
-		watcherLog("ignore: %v", strings.TrimSpace(e))
 		if strings.HasPrefix(path, strings.TrimSpace(e)) {
 			return true
 		}
